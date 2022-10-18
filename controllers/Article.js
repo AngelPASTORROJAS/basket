958e8db6757a8f8f144c315872f60e90c0d34d9f
Angel <angeldaniel.pastorrojas@gmail.com>
2022-10-18 16:07:56 +0200

    style: commentaire

--- controllers/Article.js
+++ controllers/Article.js
@@ -2,21 +2,24 @@ import { connection } from "../bdd/app.js";
 
 export default (req, res) => {
   let id = req.params.id;
-  let requete = `SELECT 
-  a.id, 
-  a.name, 
-  a.description, 
-  a.date,
-  c.id as commentaireId,
-  c.pseudo,
-  c.commentaire,
-  c.date as commentaireDate
-  FROM articles as a
-  LEFT JOIN commentaire as c ON a.id = c.articlesId 
-  WHERE a.id = ? `;
+  let requeteArticles = `SELECT * FROM articles WHERE id = ? `;
+//   let requeteCommentaire = `SELECT * FROM commentaire WHERE articlesId = ? `;
 
-  connection.query(requete, [id], function (error, articles, fields) {
-    console.log(articles); //object OBJECT Array iterator
-    res.render("article.ejs", { article: articles[0] });
+//   let article;
+//   let commentaires;
+
+  connection.query(requeteArticles, [id], function (error, articles, fields) {
+    let article = articles[0];
+    res.render("article.ejs", { article: article });
   });
+
+//   connection.query(
+//     requeteCommentaire,
+//     [id],
+//     function (error, commentairesOut, fields) {
+//       commentaires = commentairesOut;
+//       console.log(commentaires)
+//     }
+//   );
+
 };
